3dbe7f369cc5fb0c7da0c074507bb80dc0fec385
Diyar Faraj <diyar.faraj@gmail.com>
2023-09-18 23:31:05 +0200

    debug changes

--- client/src/util/idxToContinent.js
+++ client/src/util/idxToContinent.js
@@ -1,16 +1,19 @@
 export const arrContinent = [
-    'europe',
-    'asia',
-    'africa',
-    'south america',
-    'north america',
-    'oceania'
-]
+  "europe",
+  "asia",
+  "africa",
+  "south america",
+  "north america",
+  "oceania",
+];
 
 export const continentToIdx = (continent) => {
-    return arrContinent.findIndex((cont) => cont.toLowerCase() === continent.toLowerCase())
-}
+  var res = arrContinent.findIndex(
+    (cont) => cont.toLowerCase() === continent.toLowerCase()
+  );
+  return res;
+};
 
 export const idxToContinent = (idx) => {
-    return (arrContinent.filter((_, index) => index === Number(idx)))[0]
-}
+  return arrContinent.filter((_, index) => index === Number(idx))[0];
+};
